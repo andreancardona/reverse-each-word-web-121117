46420f295aded9314533d891b467ac4b1f30579e
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-24 16:39:22 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,1 @@
 def reverse_each_word (string)
-    words = string.split(' ')
-    reverse_string = []
-
-    words.length.times do |i|
-      reverse_string[i] = words[i].reverse
-    end
-
-
-    return reverse_string.join(" ")
-  end
