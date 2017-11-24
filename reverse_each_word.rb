a1e7eb642c0dc182b88f7984c1c0de87be83d824
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-24 17:11:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 def reverse_each_word (string)
-   split_string = string.split()
 
-new_array =split_string.collect{|word| word.reverse!}
-new_array.join(" ")
+reversed = string.split(" ").collect do |word|
+word.reverse
 end
+reversed.join(" ")
+end
+
